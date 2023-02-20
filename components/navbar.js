11b5d63e21cfa2bd66ccc45821ad6aa9ff448825
BrahimiMehdi <96663245+BrahimiMehdi@users.noreply.github.com>
2023-02-20 21:45:14 +0100

    Fixed your code spacing and issue

--- components/navbar.js
+++ components/navbar.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const navbar = () => {
   return (
-    <div className='flex justify-between w-full p-4 shadow-sm'>
-        <a className='text-2xl'>ToDo It</a>
+    <div className="flex justify-between w-full p-4 shadow-sm">
+      <a className="text-2xl">ToDo It</a>
 
-        <div className="justify-between flex gap-[50px] links">
-            <a href="/login" className='text-xl'>Login</a>
-            <a href="/signin" className='p-2 px-3 text-white shadow-md rounded-xl bg-main hover:bg-transpaent '>Sign in</a>
-        </div>
+      <div className="justify-between flex gap-[50px] links">
+        <a href="/login" className="text-xl">
+          Login
+        </a>
+        <a href="/signin" className="p-2 px-3 text-white shadow-md rounded-xl bg-main hover:bg-transpaent ">
+          Sign in
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default navbar
+export default navbar;
--- pages/signin.js
+++ pages/signin.js
@@ -1,28 +1,24 @@
-import Navbar from '../components/navbar'
-import {getAuth, signInWithPopup, GoogleAuthProvider} from 'firebase/auth'
-import {firebaseApp} from '../src/config/firebase.config'
-import async from '@firebase/util'
+import Navbar from "../components/navbar";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { firebaseApp } from "../src/config/firebase.config";
+import async from "@firebase/util";
 const signin = () => {
-    const firebaseAuth = getAuth(firebaseApp)
-    const provider = new GoogleAuthProvider();
-    const signIn= async()=>{
+  const firebaseAuth = getAuth(firebaseApp);
+  const provider = new GoogleAuthProvider();
+  const signIn = async () => {
+    const response = await signInWithPopup(firebaseAuth, provider);
+    console.log(response);
+  };
+  return (
+    <>
+      <Navbar />
+      <form action="">
+        <button className="p-3 text-white bg-main rounded-xl" onClick={() => signIn()}>
+          Sign in with google
+        </button>
+      </form>
+    </>
+  );
+};
 
-        const response = await signInWithPopup(firebaseAuth, provider)
-        console.log(response)
-
-
-    }
-    return (  
-        <>
-                <Navbar/>
-
-                <form action="">
-
-                    <button className='p-3 text-white bg-main rounded-xl' onClick={signIn()}>Sign in with google</button>
-                </form> 
-
-        </>
-    );
-}
- 
 export default signin;
